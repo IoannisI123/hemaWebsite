cba0d68b408b17fb3faa86303f576371ee5b0168
Ioannis Iordanoglou <113583318+IoannisI123@users.noreply.github.com>
2024-07-13 10:49:56 +0300

    header update

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Logo from './Logo/Logo'
-import Navbar from './Nav/Navbar'
 import Header from './Header/Header'
-import Banner from './Carousel/Carousel'
+import Navbar from './Nav/Navbar'
 import Carousel from './Carousel/Carousel'
 
 
 function App() {
   return (
     <div>
- <Logo></Logo>
+ <Header></Header>
 <Navbar></Navbar>
 <Carousel></Carousel>
 
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,12 +1,13 @@
 import { useState, useEffect } from "react";
-
+import "./Header.css"
 
 function Header(){
 
 return (
-<section>
-
-</section>
+<div className="project__logo gradient__bg">
+<li><h2>Ομάδα Μελέτης Ιστορικής Σπαθασκίας</h2></li>
+</div>
 );
 }
+
 export default Header
